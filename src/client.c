cf4cb461e94cbf68e321cb6b79ed8cfa1168fcd0
pwndorei <lkjuio3865@naver.com>
2023-11-20 17:31:19 +0900

    client-oriented: do_client_task

--- src/client.c
+++ src/client.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <signal.h>
+#include <sys/shm.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -18,14 +20,18 @@ int* shm_addr = NULL;
  * signal for sync (Client-Oriented)
  * client: SIGUSR1 to parent after writing 2 int at shm for 1 chunk
  * parent: receive SIGUSR1, increase count from 0 to 4, if(count == 4) all client write to shm(chunk i complete) send signal to server i & reset count
- * client: pause(), wait for parent's SIGUSR1
+ * client: raise(SIGSTOP), wait for parent's SIGUSR1
  * server: read chunk & write to file, send SIGUSR2 to parent
  * parent: send SIGUSR1 to all client, resume execution
 */
 
+
 void
 do_client_task(int mode)
 {
+		pid_t parent = getppid();
+		int data[2] = {0,};
+		int nbyte = 0;
 		//open p*.dat file
 		fd = open(dat[id], O_RDONLY);
 		if(fd == -1)
@@ -36,6 +42,25 @@ do_client_task(int mode)
 		if(mode == MODE_CLOR)
 		{
 				shm_addr = shmat(shmid, NULL, 0);
+				while(1)
+				{
+						nbyte = read(fd, &data, sizeof(int)*2);
+						if(nbyte == -1)
+						{
+								exit(E_READ_FILE);
+						}
+						else if(!nbyte)//end-of-file
+						{
+								close(fd);
+								shmdt(shm_addr);
+								//signal to parent
+								break;
+						}
+						shm_addr[id] = data[0];//write data
+						shm_addr[id + NODENUM] = data[1];
+						kill(parent, SIGUSR1);//send SIGUSR1 to parent, notify "data is written!"
+						raise(SIGSTOP);//stop until parent's SIGCONT
+				}
 		}
 		else if(mode == MODE_SVOR)
 		{
