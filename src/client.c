d7e92995444bca67954cb39423f0e3ca0522d5ab
sigmaith <drsoombre@naver.com>
2023-11-26 16:30:17 +0900

    시간 측정 함수 추가해봄.

--- src/client.c
+++ src/client.c
@@ -43,6 +43,12 @@ do_client_task(int mode)
 
 		if(mode == MODE_CLOR)
 		{
+			
+				#ifdef TIMES
+        			struct timeval stime,etime;
+        			int time_result;
+				#endif
+
 				shm_addr = shmat(shmid, NULL, 0);
 				while(1)
 				{
@@ -62,6 +68,22 @@ do_client_task(int mode)
 						kill(parent, SIGUSR1);//send SIGUSR1 to parent, notify "data is written!"
 						raise(SIGSTOP);//stop until parent's SIGCONT
 				}
+
+				#ifdef TIMES
+        			gettimeofday(&stime, NULL);
+				#endif
+
+				printf("**Program IO, communication and the rest\n");
+				printf("**Program IO\n");
+				printf("**Program communication\n");
+
+				#ifdef TIMES
+        		gettimeofday(&etime, NULL);
+        		time_result = etime.tv_usec - stime.tv_usec;
+        		printf("Client_oriented_io TIMES == %ld %ld %ld\n", etime.tv_usec, stime.tv_usec, time_result);
+				#endif
+
+
 		}
 		else if(mode == MODE_SVOR)
 		{
