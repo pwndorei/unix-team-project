6e8dd7d5b08c63f9976215f27fe88b9ee2146ab3
김근영 <76671368+Halfmoonbear422@users.noreply.github.com>
2023-11-26 16:16:26 +0900

    Update client.c

--- src/client.c
+++ src/client.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include <signal.h>
 #include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/types.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include "project.h"
 
 extern int shmid;
-const char *dat[4] = {"p1.dat", 
-					  "p2.dat", 
-					  "p3.dat", 
-					  "p4.dat"};
+extern int msgqid;
+
+const char* dat[4] = { "p1.dat",
+					  "p2.dat",
+					  "p3.dat",
+					  "p4.dat" };
 
 int fd = -1;//fd for p*.dat
 extern int id;// node's id, common.c
@@ -29,91 +34,92 @@ int* shm_addr = NULL;
 void
 do_client_task(int mode)
 {
-		pid_t parent = getppid();
-		int data[2] = {0,};
-		int nbyte = 0;
-		//open p*.dat file
-		fd = open(dat[id], O_RDONLY);
-		if(fd == -1)
-		{
-				exit(E_OPEN_FILE);
-		}
+	pid_t parent = getppid();
+	int data[2] = { 0, };
+	int nbyte = 0;
+	int i, msgi, msgid[4];
+	msgbuf msg;
+	//open p*.dat file
+	fd = open(dat[id], O_RDONLY);
+	if (fd == -1)
+	{
+		exit(E_OPEN_FILE);
+	}
 
-		if(mode == MODE_CLOR)
+	if (mode == MODE_CLOR)
+	{
+		shm_addr = shmat(shmid, NULL, 0);
+		while (1)
 		{
-				shm_addr = shmat(shmid, NULL, 0);
-				while(1)
-				{
-						nbyte = read(fd, &data, sizeof(int)*2);
-						if(nbyte == -1)
-						{
-								exit(E_READ_FILE);
-						}
-						else if(!nbyte)//end-of-file
-						{
-								close(fd);
-								shmdt(shm_addr);
-								//signal to parent
-								break;
-						}
-						shm_addr[id] = data[0];//write data
-						shm_addr[id + NODENUM] = data[1];
-						kill(parent, SIGUSR1);//send SIGUSR1 to parent, notify "data is written!"
-						raise(SIGSTOP);//stop until parent's SIGCONT
-				}
-		}
-		else if(mode == MODE_SVOR)
-		{
-			for (i = 0; i < 4; i++)  // creating msg queue #1 ~ #4, for server #1 ~ #4
+			nbyte = read(fd, &data, sizeof(int) * 2);
+			if (nbyte == -1)
 			{
-				msgid[i] = msgget(msgqid) + i, 0644 | IPC_CREAT | IPC_EXCL);
-				if (msgid[i] == -1)
-				{
-					perror("Msg queue");
-					exit(-1);
-				}
+				exit(E_READ_FILE);
 			}
-	
-			fd = open(dat[id], O_RDONLY);
-			if (fd == -1)
+			else if (!nbyte)//end-of-file
 			{
-				perror("Open file:");
+				close(fd);
+				shmdt(shm_addr);
+				//signal to parent
+				break;
+			}
+			shm_addr[id] = data[0];//write data
+			shm_addr[id + NODENUM] = data[1];
+			kill(parent, SIGUSR1);//send SIGUSR1 to parent, notify "data is written!"
+			raise(SIGSTOP);//stop until parent's SIGCONT
+		}
+	}
+	else if (mode == MODE_SVOR)
+	{
+		for (i = 0; i < 4; i++)  // creating msg queue #1 ~ #4, for server #1 ~ #4
+		{
+			msgid[i] = msgget(msgqid) + i, 0644 | IPC_CREAT | IPC_EXCL);
+			if (msgid[i] == -1)
+			{
+				perror("Msg queue");
 				exit(-1);
 			}
-	
-			msgi = 0;
-	
-			while (1)  // send data to msg queue #1 ~ #4
-			{
-				nbyte = read(fd, &data, sizeof(int) * 2);
-				if (nbyte == -1)
-				{
-					perror("Read file");
-					exit(-1);
-				}
-				else if (!nbyte)//end-of-file
-				{
-					close(fd);  // close client's own file
-					for (i = 0; i < 4; i++)  // delete four msg queues
-						msgctl(msgid[i], IPC_RMID, (struct msqid_ds*)NULL);
-	
-					//signal to parent
-					break;
-				}
-				// send two data
-				msg.mtext[0] = data[0];
-				msg.mtype = id;
-				msgsnd(msgid[msgi], msg, sizeof(int), 0);
-				msg.mtext[0] = data[1];
-				msg.mtype = id + NODENUM;
-				msgsnd(msgid[msgi++], msg, sizeof(int), 0);
-				msgi %= NODENUM;
-	
-				kill(parent, SIGUSR1);//send SIGUSR1 to parent, notify "data is written!"
-				raise(SIGSTOP);//stop until parent's SIGCONT
-	
-			}
 		}
-		exit(SUCCESS);
-}
 
+		fd = open(dat[id], O_RDONLY);
+		if (fd == -1)
+		{
+			perror("Open file:");
+			exit(-1);
+		}
+
+		msgi = 0;
+
+		while (1)  // send data to msg queue #1 ~ #4
+		{
+			nbyte = read(fd, &data, sizeof(int) * 2);
+			if (nbyte == -1)
+			{
+				perror("Read file");
+				exit(-1);
+			}
+			else if (!nbyte)//end-of-file
+			{
+				close(fd);  // close client's own file
+				for (i = 0; i < 4; i++)  // delete four msg queues
+					msgctl(msgid[i], IPC_RMID, (struct msqid_ds*)NULL);
+
+				//signal to parent
+				break;
+			}
+			// send two data
+			msg.mtext[0] = data[0];
+			msg.mtype = id;
+			msgsnd(msgid[msgi], msg, sizeof(int), 0);
+			msg.mtext[0] = data[1];
+			msg.mtype = id + NODENUM;
+			msgsnd(msgid[msgi++], msg, sizeof(int), 0);
+			msgi %= NODENUM;
+
+			kill(parent, SIGUSR1);//send SIGUSR1 to parent, notify "data is written!"
+			raise(SIGSTOP);//stop until parent's SIGCONT
+
+		}
+	}
+	exit(SUCCESS);
+}
